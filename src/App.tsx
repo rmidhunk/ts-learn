54fe3d5c2f8d173ec0847e0bbc2fb75276e0c4cc
GitofMidhun <midhun.steyp@gmail.com>
2023-07-23 13:12:02 +0530

    intial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    const name: string = "Midhun";
+    return <div className="App">{name}</div>;
+}
+
+export default App;
